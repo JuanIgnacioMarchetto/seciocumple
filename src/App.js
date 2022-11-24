de5d53bf97ff35bf663e07d51527631171ea19d5
JuanIgnacioMarchetto <43477348+JuanIgnacioMarchetto@users.noreply.github.com>
2022-11-24 10:14:20 -0300

    sesiocumple

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+      <header className="App-header">
+       <div>
+      
+        
+        <div class="container">
+            <div class="balloon">
+                <div><span>S</span>
+                </div>
+                <div><span>E</span>
+                </div>
+                <div><span>C</span>
+                </div>
+                <div><span>I</span>
+                </div>
+                <div><span>O</span>
+                </div>
+                <div><span>!</span>
+                </div>
+            </div>
+      <h1>Feli Pumpleaño!!</h1>
+        <h2 >te  quiero mucho hermanito</h2>
+        </div>
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
